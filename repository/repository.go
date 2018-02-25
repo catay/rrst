a90ee29574c381e1657e9a652dda0f6dae60568e
Steven Mertens <steven.mertens@catay.be>
2018-02-25 22:32:22 +0100

    First prototype of downloading the rpm packages into a local repo.

--- repository/repository.go
+++ repository/repository.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"github.com/catay/rrst/api/suse"
 	"github.com/catay/rrst/repomd"
+	"io"
+	"net/http"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
@@ -21,6 +24,7 @@ type Repository struct {
 	UpdateSuffix    string   `yaml:"update_suffix"`
 	CacheDir        string   `yaml:"cache_dir"`
 	IncludePatterns []string `yaml:"include_patterns"`
+	secret          string
 }
 
 func NewRepository(name string) (r *Repository) {
@@ -43,7 +47,6 @@ func (self *Repository) GetRegCode() (string, bool) {
 }
 
 func (self *Repository) Sync() error {
-	var secret string
 
 	if self.Vendor == "SUSE" {
 		fmt.Println("  - Fetch SUSE products json if older then x hours")
@@ -60,14 +63,14 @@ func (self *Repository) Sync() error {
 
 		fmt.Println("  - Get secret hash for give URL repo")
 
-		secret, ok = scc.GetSecretURI(self.RemoteURI)
+		self.secret, ok = scc.GetSecretURI(self.RemoteURI)
 		if !ok {
 			return errors.New(fmt.Sprintf("Secret for url  %v not found", self.RemoteURI))
 		}
 	}
 
 	fmt.Println("  - Fetch repomd xml file")
-	rm := repomd.NewRepoMd(self.RemoteURI, secret, self.CacheDir+"/"+self.Name)
+	rm := repomd.NewRepoMd(self.RemoteURI, self.secret, self.CacheDir+"/"+self.Name)
 
 	if err := rm.Metadata(); err != nil {
 		return err
@@ -92,7 +95,15 @@ func (self *Repository) Sync() error {
 				rpms[i].ToDownload = true
 			}
 
-			fmt.Println("  - ", rpms[i].Loc.Path, rpms[i].ToDownload)
+			//		fmt.Println("  - ", rpms[i].Loc.Path, rpms[i].ToDownload)
+		}
+	}
+
+	for _, p := range rpms {
+		if p.ToDownload {
+			if err := self.downloadPackage(p); err != nil {
+				return err
+			}
 		}
 	}
 
@@ -137,4 +148,59 @@ func (self *Repository) packageExistsLocal(p repomd.RpmPackage) (bool, error) {
 	}
 
 	return false, err
+}
+
+func (self *Repository) downloadPackage(p repomd.RpmPackage) error {
+
+	rpmDir := self.LocalURI + "/" + filepath.Dir(p.Loc.Path)
+	remoteRpmPath := self.RemoteURI + "/" + p.Loc.Path + "?" + self.secret
+	localRpmPath := self.LocalURI + "/" + p.Loc.Path
+
+	// create dir
+	if err := os.MkdirAll(rpmDir, 0755); err != nil {
+		return err
+	}
+
+	// create file
+
+	f, err := os.Create(localRpmPath)
+	defer f.Close()
+
+	if err != nil {
+		return err
+	}
+
+	// download
+
+	client := &http.Client{}
+
+	resp, err := client.Get(remoteRpmPath)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("HTTP error %v ", resp.StatusCode))
+	}
+
+	fmt.Printf("* %s ... ", p.Loc.Path)
+
+	buf := make([]byte, 0, 1)
+	var nbytes int64
+
+	for {
+		n, err := resp.Body.Read(buf[:cap(buf)])
+		buf = buf[:n]
+		nbytes += int64(n)
+		f.Write(buf)
+
+		if err == io.EOF {
+			fmt.Println("successfull")
+			break
+		}
+	}
+
+	return nil
+
 }
