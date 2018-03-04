2c362e4425d5abc6c8b54aecdd683cc27cd81d02
Steven Mertens <steven.mertens@catay.be>
2018-03-04 21:10:36 +0100

    Implement IsRegularFile() function and ginkgo unit tests.

---
+++ util/file/file.go
@@ -0,0 +1,18 @@
+package file
+
+import (
+	"os"
+)
+
+func IsRegularFile(name string) bool {
+	fi, err := os.Stat(name)
+
+	if err != nil {
+		return false
+	}
+
+	if fi.Mode().IsRegular() {
+		return true
+	}
+	return false
+}
---
+++ util/file/file_test.go
@@ -0,0 +1,60 @@
+package file_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/catay/rrst/util/file"
+)
+
+// Feature: file package
+// Scenario: testing IsRegularFile(name string) function
+
+// Given a function IsRegularFile(name string)
+// When passing an existing regular file as argument
+// Then the function should return true
+
+// When passing a non-existing regular file as argument
+// Then the function should return false
+
+// When passing a existing directory as argument
+// Then the function should return false
+
+var _ = Describe("File package: ", func() {
+	var (
+		existingDirName     string
+		existingFileName    string
+		nonExistingDirName  string
+		nonExistingFileName string
+	)
+
+	BeforeEach(func() {
+		existingDirName = "testdata/this_is_a_directory"
+		existingFileName = existingDirName + "/this_is_a_regular_file.txt"
+		nonExistingDirName = "testdata/this_is_a_not_existing_directory"
+		nonExistingFileName = nonExistingDirName + "/this_is_a_not_existing_regular_file.txt"
+	})
+
+	Describe("Given a function IsRegularFile(name string)", func() {
+		Context("when passing an existing regular file as argument", func() {
+			It("should return true", func() {
+				v := IsRegularFile(existingFileName)
+				Expect(v).To(BeTrue())
+			})
+		})
+
+		Context("when passing a non-existing regular file as argument", func() {
+			It("should return false", func() {
+				v := IsRegularFile(nonExistingFileName)
+				Expect(v).To(BeFalse())
+			})
+		})
+
+		Context("when passing an existing directory as argument", func() {
+			It("should return false", func() {
+				v := IsRegularFile(existingDirName)
+				Expect(v).To(BeFalse())
+			})
+		})
+	})
+})
