4c889716bf564a2785a7ce8c8ed368c4590c0292
Steven Mertens <steven.mertens@catay.be>
2018-12-29 23:39:14 +0100

    Define provider specific settings in the configuration.
    Provider contains provider specific configuration settings.
    For example custom authentication setups for provider enterprise repo's.

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 // Default configuration values.
@@ -30,8 +31,9 @@ type Config struct {
 
 // GlobalConfig contains the global configuration settings.
 type GlobalConfig struct {
-	ContentPath        string `yaml:"content_path"`
-	MaxRevisionsToKeep int    `yaml:"max_tags_to_keep"`
+	ContentPath        string      `yaml:"content_path"`
+	Providers          []*Provider `yaml:"providers"`
+	MaxRevisionsToKeep int         `yaml:"max_tags_to_keep"`
 }
 
 // RepositoryConfig contains the per repository configuration settings.
@@ -39,7 +41,7 @@ type RepositoryConfig struct {
 	Id                 int      `yaml:"id"`
 	Name               string   `yaml:"name"`
 	RType              string   `yaml:"type"`
-	Vendor             string   `yaml:"vendor"`
+	ProviderId         string   `yaml:"provider_id"`
 	RegCode            string   `yaml:"reg_code"`
 	RemoteURI          string   `yaml:"remote_uri"`
 	ContentSuffixPath  string   `yaml:"content_suffix_path"`
@@ -50,6 +52,24 @@ type RepositoryConfig struct {
 	ContentMDPath      string
 	ContentTagsPath    string
 	ContentTmpPath     string
+	Provider           *Provider
+}
+
+// Provider contains provider specific configuration settings.
+//
+// Currently only SUSE SCC credentials are supported.
+// Id has to contain a free to choose name to which a  repo can map too.
+// Name should be the provider name, for example SUSE.
+// The variables can contain log in credentials. In the case of SUSE SCC
+// it needs to contain for example the variable scc_reg_code which should
+// contain a SCC registration code.
+type Provider struct {
+	Id        string `yaml:"id"`
+	Name      string `yaml:"provider"`
+	Variables []struct {
+		Name  string `yaml:"name"`
+		Value string `yaml:"value"`
+	} `yaml:"variables"`
 }
 
 // NewConfig loads the configuration from a YAML file and returns it.
@@ -71,6 +91,11 @@ func NewConfig(configFile string) (c *Config, err error) {
 	// Load the configuration values from the YAML config file.
 	if err := c.LoadFromYAMLFile(configFile); err != nil {
 		return nil, fmt.Errorf("error loading config: %s", err)
+	}
+
+	// Substitute substitution environment variables when present.
+	for i, _ := range c.GlobalConfig.Providers {
+		c.GlobalConfig.Providers[i].SetEnvVars()
 	}
 
 	// Set repository configuration defaults when not set after
@@ -112,5 +137,48 @@ func (c *Config) SetRepositoryConfigDefaults() {
 		c.RepoConfigs[i].ContentMDPath = c.GlobalConfig.ContentPath + "/" + DefaultContentMDPathSuffix + "/" + r.ContentSuffixPath
 		c.RepoConfigs[i].ContentTagsPath = c.GlobalConfig.ContentPath + "/" + DefaultContentTagsPathSuffix + "/" + r.ContentSuffixPath
 		c.RepoConfigs[i].ContentTmpPath = c.GlobalConfig.ContentPath + "/" + DefaultContentTmpPathSuffix + "/" + r.ContentSuffixPath
+
+		// reference repo provider id to provider data if present and match found.
+		for _, p := range c.GlobalConfig.Providers {
+			if r.ProviderId == p.Id {
+				c.RepoConfigs[i].Provider = p
+			}
+		}
 	}
+}
+
+// SetEnvVars checks if the value of a provider variable references a
+// environment variable and does the substitution when present
+// If not present the original value is retained.
+func (p *Provider) SetEnvVars() {
+	for i, v := range p.Variables {
+		if IsEnvVar(v.Value) {
+			value, ok := EnvVarValue(v.Value)
+			if !ok {
+				fmt.Printf("config: warning: no env var set for %v\n", v.Name)
+			}
+			p.Variables[i].Value = value
+		}
+	}
+}
+
+// EnvVarValue returns the value of the environment variable.
+// If environment value is not set, the bool will be false.
+func EnvVarValue(v string) (string, bool) {
+	if strings.HasPrefix(v, "${") && strings.HasSuffix(v, "}") {
+		key := strings.TrimPrefix(v, "${")
+		key = strings.TrimSuffix(key, "}")
+		return os.LookupEnv(key)
+	}
+
+	return v, false
+}
+
+// IsEnvVar checks if the value is a substitution variable.
+// Returns true or false.
+func IsEnvVar(v string) bool {
+	if strings.HasPrefix(v, "${") && strings.HasSuffix(v, "}") {
+		return true
+	}
+	return false
 }
